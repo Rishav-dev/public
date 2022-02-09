875aa93040270991c80d712fdccccdf11554af1c
Rishav-dev <83537945+Rishav-dev@users.noreply.github.com>
2022-02-09 10:22:37 +0530

    Added irl pictures and created a join page

---
+++ join.js
@@ -0,0 +1,27 @@
+
+   
+const form = document.querySelector('#contactForm');
+
+
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  function submitForm(e){
+    e.preventDefault();
+
+
+    db.collection('messages').add({
+        name: form.name.value,
+        email: form.email.value,
+        help: form.help.value,
+        subject: form.subject.value
+    });
+    form.name.value = '';
+    form.email.value = '';
+    form.help.value = '';
+    form.subject.value = '';
+  }
+  
+
+  
+
+  
